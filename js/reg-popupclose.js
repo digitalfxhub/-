2bce47c8566e5a8c67f7bae85654e8dec557cf50
digitalfxhub <viralkaro.shop@gmail.com>
2025-01-06 15:53:42 +0530

    Create reg-popupclose.js

---
+++ js/reg-popupclose.js
@@ -0,0 +1,10 @@
+// Function to open the popup
+function openPopup() {
+  document.getElementById('registrationPopup').style.display = 'block';
+}
+
+// Function to close the popup and refresh the page
+function closeregopup() {
+  document.getElementById('registrationPopup').style.display = 'none';
+  location.reload(); // Refresh the page
+}
